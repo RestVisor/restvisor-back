67be11b00812bc6ad5b67a71b628baa60da9bc4d
ElenaArtiles <morartielen@gmail.com>
2025-03-25 17:33:24 +0000

    Implement product deletion

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getPedidos, createPedido, getProducts, createProducts} = require("../controllers/productsController");
+const { getPedidos, createPedido, getProducts, createProducts, deleteProduct} = require("../controllers/productsController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 router.get("/", authMiddleware, getProducts);
 router.post("/", authMiddleware, createProducts);
+router.post("/:id", authMiddleware, deleteProduct);
 
 module.exports = router;
